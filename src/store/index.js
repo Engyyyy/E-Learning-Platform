f175d4f0ab19f9bdcad984fde1786e460b90ecd2
Engy <engeousthepotionmaster@gmail.com>
2023-08-11 01:39:15 +0300

    courses requests handled

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { usersApi } from "./apis/usersApi";
+import { coursesApi } from "./apis/coursesApi";
 import { loginSlice } from "./slices/loginSlice";
 
 const store = configureStore({
   reducer: {
     isLoggedIn: loginSlice.reducer,
     [usersApi.reducerPath]: usersApi.reducer,
-    //courses: coursesApi.reducer
+    [coursesApi.reducerPath]: coursesApi.reducer,
   },
   middleware(getDefaultMiddleware) {
-    return getDefaultMiddleware().concat(usersApi.middleware);
+    return getDefaultMiddleware()
+      .concat(usersApi.middleware)
+      .concat(coursesApi.middleware);
   },
 });
 
@@ -19,3 +22,4 @@ setupListeners(store.dispatch);
 export { store };
 export * from "./apis/usersApi";
 export * from "./slices/loginSlice";
+export * from "./apis/coursesApi";
