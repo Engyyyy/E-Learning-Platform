b85e9e5b83c055ed81604d84dae1e6ffeb74d4f0
Engy <engeousthepotionmaster@gmail.com>
2023-08-14 12:25:02 +0300

    course banner image

--- src/components/course-details/CourseBanner.js
+++ src/components/course-details/CourseBanner.js
@@ -37,49 +37,55 @@ function CourseBanner({ course }) {
   );
   return (
     <Stack
-      sx={{
-        backgroundColor: "#28A19C",
-        padding: "30px 40px",
-      }}
+      sx={{ backgroundColor: "#28A19C" }}
+      direction="row"
+      justifyContent="space-between"
     >
-      <Stack spacing="15px">
-        <Typography color="white" component="span" variant="h2">
-          {course.classified_product.title}
-        </Typography>
-        <Typography color="white" fontSize="16px" fontWeight={500}>
-          {course.classified_product.description}
-        </Typography>
-        <Stack direction="row" spacing="60px">
-          <Review
-            rate={course.final_rating_from_reviews}
-            votes={course.reviews_number}
-            rateColor="white"
-            rateFontSize="19px"
-            votesColor="white"
-            votesFontSize="14px"
-          />
-          <Stack direction="row" alignItems="center" spacing="5px">
-            <img width="20px" height="20px" src={InfoIcon} alt="info-icon" />
-            <Typography fontSize="16px" fontWeight={500} color="white">
-              Last Updated 2/3/2019
-            </Typography>
-          </Stack>
-          <Stack direction="row" alignItems="center" spacing="5px">
-            <img
-              width="20px"
-              height="20px"
-              src={LanguageIcon}
-              alt="language-icon"
+      <Stack
+        sx={{
+          padding: "30px 40px",
+        }}
+      >
+        <Stack spacing="15px">
+          <Typography color="white" component="span" variant="h2">
+            {course.classified_product.title}
+          </Typography>
+          <Typography color="white" fontSize="16px" fontWeight={500}>
+            {course.classified_product.description}
+          </Typography>
+          <Stack direction="row" spacing="60px">
+            <Review
+              rate={course.final_rating_from_reviews}
+              votes={course.reviews_number}
+              rateColor="white"
+              rateFontSize="19px"
+              votesColor="white"
+              votesFontSize="14px"
             />
-            <Typography fontSize="16px" fontWeight={500} color="white">
-              English
-            </Typography>
+            <Stack direction="row" alignItems="center" spacing="5px">
+              <img width="20px" height="20px" src={InfoIcon} alt="info-icon" />
+              <Typography fontSize="16px" fontWeight={500} color="white">
+                Last Updated 2/3/2019
+              </Typography>
+            </Stack>
+            <Stack direction="row" alignItems="center" spacing="5px">
+              <img
+                width="20px"
+                height="20px"
+                src={LanguageIcon}
+                alt="language-icon"
+              />
+              <Typography fontSize="16px" fontWeight={500} color="white">
+                English
+              </Typography>
+            </Stack>
           </Stack>
         </Stack>
+        <Stack flexWrap="wrap" direction="row" paddingTop="20px">
+          {renederedInstructors}
+        </Stack>
       </Stack>
-      <Stack flexWrap="wrap" direction="row" paddingTop="20px">
-        {renederedInstructors}
-      </Stack>
+      <img src={course.classified_product.image_url} alt="course" />
     </Stack>
   );
 }
